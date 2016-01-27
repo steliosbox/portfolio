bf057d9ba3ebdc0df8f8969dbe7bad23c1d59454
Stelios Baglaridis <Stelios Baglaridis>
2016-01-28 01:51:00 +0200

    some corrections

--- README.md
+++
@@ -1,7 +0,0 @@
-How to install my project
-
-1. git clone http://githib.com/steliosbox/portfolio.gti
-2. cd portfolio
-3. bower i
-4. npm i
-5. gulp i
--- gulpfile.js
+++ gulpfile.js
@@ -16,19 +16,12 @@ gulp.task('watch', function () {
   gulp.watch([
     'app/*.html',
     'app/js/**/*.js',
-    'app/css/**/*.css'
+    'app/css/*.css',
+	'app/css/*.scss'
   ]).on('change', browserSync.reload);
 });
 
 // Задача по-умолчанию
 gulp.task('default', ['server', 'watch']);
 
-gulp.task('sass', function () {
 
-  gulp.src('app/assets/sass/styles.sass')
-    .pipe(sass({
-        errLogToConsole: true,
-        sourceComments : 'normal'
-    }))
-    .pipe(gulp.dest('public_html/assets/css'))
-});
